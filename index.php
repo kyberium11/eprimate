4b5788934e36e17d12d0a134a562b8e81dccca6d
kyberium11 <brojeromealtarejos@gmail.com>
2022-07-09 21:21:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+	<div id="primary" <?php astra_primary_class(); ?>>
+		<?php
+		astra_primary_content_top();
+
+		astra_content_loop();
+
+		astra_pagination();
+
+		astra_primary_content_bottom();
+		?>
+	</div><!-- #primary -->
+<?php
+if ( astra_page_layout() == 'right-sidebar' ) :
+
+	get_sidebar();
+
+endif;
+
+get_footer();
